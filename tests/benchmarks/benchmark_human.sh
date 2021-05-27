a78bd9089c63912fa6868e050e6775d8139951e9
Marco Oliva <marco.oliva@ufl.edu>
2021-05-27 16:07:29 -0400

    Still working on benchmarks

--- tests/benchmarks/benchmark_human.sh
+++ tests/benchmarks/benchmark_human.sh
@@ -3,6 +3,7 @@
 #SBATCH --account=boucher
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=marco.oliva@ufl.edu
+#SBATCH --exclusive
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=60gb
@@ -25,4 +26,9 @@ module load htslib
 module load bcftools
 module load git
 
+# 10 samples
 ${PROFILER} ${BENCHMARK} -t 32 -s ${SAMPLES_LIST_FILE}
+
+# 100 samples
+
+# 200 samples
