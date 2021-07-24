54b7b9a439f4195af99d207f5e038c150863fe38
Marco Oliva <marco.oliva@ufl.edu>
2021-07-24 15:37:17 -0400

    Update benchmarks

--- tests/benchmarks/benchmark_human_and_arabidopsis.sh
+++ tests/benchmarks/benchmark_human_and_arabidopsis.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=ex-HeA
+#SBATCH --job-name=exp-A
 #SBATCH --account=boucher
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=marco.oliva@ufl.edu
 #SBATCH --exclusive
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=120
 #SBATCH --mem=1000gb
 #SBATCH --time=240:00:00
-#SBATCH --output=%j_ex-HeA.log
+#SBATCH --output=%j_exp-A.log
 #SBATCH --constraint='hpg3&amd&rome&infiniband'
 #
 # Asking for hpg-default 	128 	8 	16 	1 	1028 	hpg3;amd;rome;infiniband 	AMD EPYC 7702 64-Core Processor
@@ -26,16 +26,16 @@ fi
 
 ##----------------------------------------------------------
 # Setup
-BASE_DIR_EXP="/blue/boucher/marco.oliva/projects/experiments/pfp"
+BASE_DIR_EXP="${BASE_DIR_EXP}"
 PROFILER="/usr/bin/time --verbose"
 
-BENCHMARK_HUMAN="/blue/boucher/marco.oliva/projects/experiments/pfp/repo/pfp/tests/benchmarks/benchmark_human.py"
-SAMPLES_LIST_BASE_HUMAN="/blue/boucher/marco.oliva/projects/experiments/pfp/repo/pfp/tests/benchmarks/samples_lists/input_list"
-DATA_HUMAN="/blue/boucher/marco.oliva/projects/experiments/pfp/human_tests/data/samples/samples"
+BENCHMARK_HUMAN="${BASE_DIR_EXP}/repo/pfp/tests/benchmarks/benchmark_human.py"
+SAMPLES_LIST_BASE_HUMAN="${BASE_DIR_EXP}/repo/pfp/tests/benchmarks/samples_lists/input_list"
+DATA_HUMAN="${BASE_DIR_EXP}/human_tests/data/samples"
 
-BENCHMARK_ARABIDOPSIS="/blue/boucher/marco.oliva/projects/experiments/pfp/repo/pfp/tests/benchmarks/benchmark_arabidopsis.py"
-SAMPLES_LIST_BASE_ARABIDOPSIS="/blue/boucher/marco.oliva/projects/experiments/pfp/repo/pfp/tests/benchmarks/samples_lists/arabidopsis_input_list"
-DATA_ARABIDOPSIS="/blue/boucher/marco.oliva/projects/experiments/pfp/arabidopsis_tests/data/samples/samples"
+BENCHMARK_ARABIDOPSIS="${BASE_DIR_EXP}/repo/pfp/tests/benchmarks/benchmark_arabidopsis.py"
+SAMPLES_LIST_BASE_ARABIDOPSIS="${BASE_DIR_EXP}/repo/pfp/tests/benchmarks/samples_lists/arabidopsis_input_list"
+DATA_ARABIDOPSIS="${BASE_DIR_EXP}/arabidopsis_tests/data/samples"
 
 module load python/3.6
 module load htslib
@@ -51,9 +51,9 @@ module load gcc/9.3.0
 #${PROFILER} ${BENCHMARK_HUMAN} -t 32 -d ${DATA_HUMAN} -s "${SAMPLES_LIST_BASE_HUMAN}_1600.txt" --skip-pscan
 #${PROFILER} ${BENCHMARK_HUMAN} -t 32 -d ${DATA_HUMAN} -s "${SAMPLES_LIST_BASE_HUMAN}_all.txt" --skip-pscan
 
-${PROFILER} ${BENCHMARK_ARABIDOPSIS} -t 32 -d ${DATA_ARABIDOPSIS} -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_25.txt"
-${PROFILER} ${BENCHMARK_ARABIDOPSIS} -t 32 -d ${DATA_ARABIDOPSIS} -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_125.txt"
-${PROFILER} ${BENCHMARK_ARABIDOPSIS} -t 32 -d ${DATA_ARABIDOPSIS} -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_250.txt"
-${PROFILER} ${BENCHMARK_ARABIDOPSIS} -t 32 -d ${DATA_ARABIDOPSIS} -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_500.txt"
-${PROFILER} ${BENCHMARK_ARABIDOPSIS} -t 32 -d ${DATA_ARABIDOPSIS} -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_1000.txt"
+${PROFILER} "${BENCHMARK_ARABIDOPSIS}" -t 32 -d "${DATA_ARABIDOPSIS}" -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_25.txt"
+${PROFILER} "${BENCHMARK_ARABIDOPSIS}" -t 32 -d "${DATA_ARABIDOPSIS}" -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_125.txt"
+${PROFILER} "${BENCHMARK_ARABIDOPSIS}" -t 32 -d "${DATA_ARABIDOPSIS}" -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_250.txt"
+${PROFILER} "${BENCHMARK_ARABIDOPSIS}" -t 32 -d "${DATA_ARABIDOPSIS}" -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_500.txt"
+${PROFILER} "${BENCHMARK_ARABIDOPSIS}" -t 32 -d "${DATA_ARABIDOPSIS}" -s "${SAMPLES_LIST_BASE_ARABIDOPSIS}_1000.txt"
 
