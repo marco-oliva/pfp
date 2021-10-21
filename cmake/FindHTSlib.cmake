cd86891cb9b5826a854050389dba485efd71a91b
Marco Oliva <marco.oliva@ufl.edu>
2021-10-21 11:03:32 -0400

    Update FindHTSlib.cmake

--- cmake/FindHTSlib.cmake
+++ cmake/FindHTSlib.cmake
@@ -11,10 +11,12 @@ include(LibFindMacros)
 libfind_package(HTSLib ZLIB REQUIRED)
 libfind_package(HTSLib LibLZMA REQUIRED)
 libfind_package(HTSLib BZip2 REQUIRED)
+libfind_package(HTSLib CURL REQUIRED)
 
 include_directories(${LIBLZMA_INCLUDE_DIRS})
 include_directories(${BZIP2_INCLUDE_DIR})
 include_directories(${ZLIB_INCLUDE_DIRS})
+include_directories(${CURL_INCLUDE_DIRS})
 
 #target_link_libraries(DukasCompiler ${LIBLZMA_LIBRARIES})
 
@@ -36,4 +38,4 @@ find_library(HTSlib_LIBRARY
 
 libfind_process(HTSlib)
 
-set(HTSlib_LIBRARIES ${HTSlib_LIBRARIES} ${LIBLZMA_LIBRARIES} ${BZIP2_LIBRARIES} ${ZLIB_LIBRARIES})
+set(HTSlib_LIBRARIES ${HTSlib_LIBRARIES} ${LIBLZMA_LIBRARIES} ${BZIP2_LIBRARIES} ${ZLIB_LIBRARIES} ${CURL_LIBRARIES})
