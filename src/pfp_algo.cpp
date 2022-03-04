7ab839530469fb3e24f7c4d0f94e39dcf3c07804
maxrossi91 <maxrossi91@gmail.com>
2022-03-04 11:42:38 -0500

    Fix double w  when using -H 12

--- src/pfp_algo.cpp
+++ src/pfp_algo.cpp
@@ -307,7 +307,7 @@ vcfbwt::pfp::ParserVCF::operator()(const vcfbwt::Sample& sample)
     {
         // Append w dollar prime at the end of each sample, also w DOLLAR if it's the last sample
         phrase.append(this->w - 1, DOLLAR_PRIME);
-        if (sample.last()) { phrase.append(this->w, DOLLAR); }
+        if (sample.last(this->working_genotype)) { phrase.append(this->w, DOLLAR); }
         else { phrase.append(1, DOLLAR_SEQUENCE); }
 
         hash_type hash = this->dictionary->check_and_add(phrase);
